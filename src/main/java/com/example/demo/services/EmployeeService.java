279d7b5b0e9f9f5ce3835ccc1ecd3b5f1d619922
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2023-05-16 15:54:13 +0100

    start spring boot projet with thymeleaf

---
+++ src/main/java/com/example/demo/services/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import com.example.demo.entities.Employee;
+
+public interface EmployeeService {
+    List < Employee > getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+}
